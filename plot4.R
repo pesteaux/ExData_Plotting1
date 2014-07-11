f7fdaa4e6da9cf976d00c16dc33b71e998990919
pesteaux <dawjer@gmail.com>
2014-07-11 14:08:45 -0500

    modified:   plot4.R 	Fixed a typo and a plot scale

--- plot4.R
+++ plot4.R
@@ -28,24 +28,24 @@ toplot$value<-as.numeric(as.character(toplot$value))
 
 plot2<-ggplot(data=hpcdf,aes(x=dtime,y=gap)) + geom_line() + xlab("") + ylab("GLobal Active Power (kilowatts)")
 plot2<-plot2 + scale_x_datetime(breaks="1 day",labels=date_format("%a")) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-  panel.background = element_blank(), axis.line = element_line(color = "black"),
-  legend.background=element_rect(color="black"),axis.text.x = element_text(color="black"),
-  axis.text.y = element_text(color="black"))
+                                                                                 panel.background = element_blank(), axis.line = element_line(color = "black"),
+                                                                                 legend.background=element_rect(color="black"),axis.text.x = element_text(color="black"),
+                                                                                 axis.text.y = element_text(color="black"))
 
 plot3<-ggplot(toplot, aes(x=time, y=value, group=variable, colour=variable )) + geom_line()
 plot3 <- plot3 + scale_y_discrete(breaks=c("0","10","20","30")) + ylab("Energy sub metering")
 plot3 <- plot3 + scale_x_datetime(breaks="1 day",labels=date_format("%a")) + xlab("")
 plot3 <- plot3 + scale_colour_manual(values=c("black","red","blue"))
 plot3 <- plot3 + theme(legend.position=c(.80,.80),
-                 legend.background = element_rect(size = 0),
-                 legend.title=element_blank(),
-                 panel.grid.major = element_blank(), 
-                 panel.grid.minor = element_blank(), 
-                 panel.background = element_blank(), axis.line = element_line(color = "black"),
-                 axis.text.x = element_text(color="black"),
-                 axis.text.y = element_text(color="black"))
+                       legend.background = element_rect(size = 0),
+                       legend.title=element_blank(),
+                       panel.grid.major = element_blank(), 
+                       panel.grid.minor = element_blank(), 
+                       panel.background = element_blank(), axis.line = element_line(color = "black"),
+                       axis.text.x = element_text(color="black"),
+                       axis.text.y = element_text(color="black"))
 
-plot4a<-ggplot(data=hpcdf,aes(x=dtime,y=grap)) + geom_line() + xlab("datetime") + ylab("GLobal Rective Power (kilowatts)")
+plot4a<-ggplot(data=hpcdf,aes(x=dtime,y=grap)) + geom_line() + xlab("datetime") + ylab("Global Rective Power (kilowatts)")
 plot4a<-plot4a + scale_x_datetime(breaks="1 day",labels=date_format("%a")) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
   panel.background = element_blank(), axis.line = element_line(color = "black"),
   legend.background=element_rect(color="black"),axis.text.x = element_text(color="black"),
@@ -56,9 +56,10 @@ plot4b<-plot4b + scale_x_datetime(breaks="1 day",labels=date_format("%a")) + the
   panel.background = element_blank(), axis.line = element_line(color = "black"),
   legend.background=element_rect(color="black"),axis.text.x = element_text(color="black"),
   axis.text.y = element_text(color="black"))
-
-png('plot4.png')
+plot4b<-plot4b + scale_y_continuous(breaks=(seq(234,246,4)))
+plot4b
+#png('plot4.png')
 
 grid.arrange(plot2,plot4b,plot3,plot4a, ncol = 2)
 
-dev.off()
+#dev.off()
